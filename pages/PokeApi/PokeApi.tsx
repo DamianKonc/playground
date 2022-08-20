2c26d6a5d47bf94934301a5f07c79d8b59cbe3e0
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 <damian.koncewicz@wp.pl>
2022-08-20 21:17:16 +0200

    playing with pokeapi v2

--- pages/PokeApi/PokeApi.tsx
+++ pages/PokeApi/PokeApi.tsx
@@ -1,7 +1,49 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const PokeApi: NextPage = () => {
-  return <div>This will be Poke Api, be patient</div>;
+  const [pokeData, setPokeData] = useState<any[]>([]);
+  const [fetchedPokeData, setfetchedPokeData] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+
+  const getData = async () => {
+    const fetchPromise = fetch("https://pokeapi.co/api/v2/pokemon/");
+    fetchPromise
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.results.name);
+        const results = data.results;
+        // console.log(results);
+        console.log(results);
+        setPokeData((prev) => [
+          { name: data.results.name, imageUrl: data.results.url },
+        ]);
+      });
+  };
+
+  console.log(pokeData);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome in PokeApi</h1>
+      <ul>
+        {/* {pokeData.map((el) => (
+          <li key={el.name}>
+            {el.name}, {el.url}
+          </li>
+        ))} */}
+      </ul>
+    </div>
+  );
 };
 
 export default PokeApi;
--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,13 +1,37 @@
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { MouseEventHandler } from "react";
 
 describe("test string home", () => {
   it("renders a home string", () => {
     render(<Home />);
 
     const heading = screen.getByRole("home");
+    const btn = screen.getByText("Guzik");
 
     expect(heading).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
   });
+});
+
+type Props = {
+  onClick: MouseEventHandler;
+  children: string;
+};
+
+const Button = ({ onClick, children }: Props) => {
+  return <button onClick={onClick}>{children}</button>;
+};
+
+test("calls onClick prop when clicked", () => {
+  const handleClick = jest.fn();
+  render(<Button onClick={handleClick}>Guzik</Button>);
+  fireEvent.click(screen.getByText("Guzik"));
+  expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
+test("deleting item", () => {
+  const deleteButtons = screen.queryAllByAltText(" Usuń mnie");
+  expect(deleteButtons).toHaveLength(0);
 });
