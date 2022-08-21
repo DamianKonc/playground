07f06bffec46117cb5816736044c0f6f89af83ce
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 <damian.koncewicz@wp.pl>
2022-08-21 12:15:22 +0200

    fixed double render problem

--- pages/PokeApi/PokeApi.tsx
+++ pages/PokeApi/PokeApi.tsx
@@ -1,11 +1,16 @@
 import { NextPage } from "next";
 import { useEffect, useState } from "react";
 
+import styles from "./PokeApi.module.scss";
+
 const PokeApi: NextPage = () => {
   const [pokemonData, setPokemonData] = useState<{}[]>([]);
+  const [pokemonApiLink, setPokemonApiLink] = useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
 
-  const getData = async () => {
-    const fetchPromise = fetch("https://pokeapi.co/api/v2/pokemon/");
+  const getData = async (pokeLink) => {
+    const fetchPromise = fetch(pokeLink);
     fetchPromise
       .then((response) => {
         if (!response.ok) {
@@ -14,36 +19,42 @@ const PokeApi: NextPage = () => {
         return response.json();
       })
       .then((data) => {
-        setPokemonData([]);
         const resp = data.results.map((el) => fetch(el.url));
-        resp.map((el) =>
-          el
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error(`HTTP error: ${response.status}`);
-              }
 
-              return response.json();
-            })
-            .then((data) => {
-              setPokemonData((prev) => [...prev, data]);
-            })
-        );
+        const addItems = async (items) => {
+          setPokemonData([]);
+          items.map((el) =>
+            el
+              .then((response) => {
+                if (!response.ok) {
+                  throw new Error(`HTTP error: ${response.status}`);
+                }
+                return response.json();
+              })
+              .then((data) => {
+                console.log(data);
+                setPokemonData((prev) => [...prev, data]);
+              })
+          );
+        };
+        addItems(resp);
       });
   };
   console.log(pokemonData);
 
   useEffect(() => {
-    getData();
-  }, []);
-
+    setPokemonData([]);
+    getData(pokemonApiLink);
+  }, [pokemonApiLink]);
+  // ........
   return (
     <div>
       <h1>Welcome in PokeApi</h1>
-      <ul>
+      <ul className={styles.pokeApi__list}>
         {pokemonData.map((el) => (
-          <li key={el.id}>
-            {el.name}, {el.url}
+          <li className={styles.pokeApi__list_el} key={el.id}>
+            <h3>{el.name}</h3>
+            <img alt={el.name} src={el.sprites.front_default} />
           </li>
         ))}
       </ul>
