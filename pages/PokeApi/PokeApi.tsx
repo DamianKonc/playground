cb23ed5e832c555ff196ec38925951ddd0098b62
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 <damian.koncewicz@wp.pl>
2022-08-22 19:35:45 +0200

    trying to fix useEffect

--- pages/PokeApi/PokeApi.tsx
+++ pages/PokeApi/PokeApi.tsx
@@ -4,7 +4,64 @@ import { useEffect, useState } from "react";
 import styles from "./PokeApi.module.scss";
 
 const PokeApi: NextPage = () => {
-  return <div>PokeApi</div>;
+  const [pokeUrl, setPokeUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [singlePokeUrl, setSinglePokeUrl] = useState<string[]>([]);
+  const [pokemonDatas, setPokemonDatas] = useState<{}[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const setFirstPokeUrl = () => {
+    setPokeUrl("https://pokeapi.co/api/v2/pokemon/");
+  };
+
+  const getUrls = () => {
+    setSinglePokeUrl([]);
+    fetch(pokeUrl)
+      .then((data) => data.json())
+      .then((data) =>
+        data.results.map((el) => setSinglePokeUrl((prev) => [...prev, el.url]))
+      );
+  };
+  //..
+  const getPokemonDatas = () => {
+    setLoading(true);
+    setPokemonDatas([]);
+    const fetchingData = () =>
+      singlePokeUrl.map((el) =>
+        fetch(el)
+          .then((item) => item.json())
+          .then((data) => {
+            setPokemonDatas((prev) => [...prev, data]);
+            setLoading(false);
+          })
+      );
+
+    setTimeout(fetchingData, 200);
+  };
+
+  useEffect(() => {
+    setFirstPokeUrl();
+    getUrls();
+  }, []);
+
+  useEffect(() => {
+    getPokemonDatas();
+  }, [pokeUrl]);
+  //.
+  return (
+    <section>
+      <ul>
+        {loading ? (
+          <div>LOADING</div>
+        ) : (
+          pokemonDatas.map((el) => (
+            <li key={el.id}>
+              {el.name} <img src={el.sprites.front_default} alt={el.name} />
+            </li>
+          ))
+        )}
+      </ul>
+    </section>
+  );
 };
 
 export default PokeApi;
