6ad47b0d58fbf1aa64e6b0f8eb28650928e0542a
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 <damian.koncewicz@wp.pl>
2022-08-23 14:52:19 +0200

    allmost

--- pages/PokeApi/poke/[name].tsx
+++ pages/PokeApi/poke/[name].tsx
@@ -1,5 +1,13 @@
+import { useRouter } from "next/router";
+
 const Details = () => {
-  return <h1>Second try</h1>;
+  const router = useRouter();
+  const fetchingName = router.query.name;
+  console.log(fetchingName);
+  fetch(`https://pokeapi.co/api/v2/pokemon/${fetchingName}`)
+    .then((data) => data.json())
+    .then((el) => console.log(el));
+  return <h1>Second try {fetchingName}</h1>;
 };
 
 export default Details;
