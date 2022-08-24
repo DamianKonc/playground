77b7394e3f86a0b7a2bcff5714bdb32a04b9f08e
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 <damian.koncewicz@wp.pl>
2022-08-24 17:16:42 +0200

    starting fun with nbp api

--- pages/NBPApi/NBPApi.tsx
+++ pages/NBPApi/NBPApi.tsx
@@ -1,7 +1,45 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const NBPApi: NextPage = () => {
-  return <div>This will be NBP Api, be patient</div>;
+  const [fetchingFrom, setFetchingFrom] = useState<string>();
+  const [datas, setDatas] = useState();
+  useEffect(() => {
+    fetch("http://api.nbp.pl/api/exchangerates/rates/C/978/")
+      .then((data) => data.json())
+      .then((json) => console.log(json));
+  }, []);
+
+  const setWhatYouWant = (e) => {
+    console.log(e.target.value);
+    setFetchingFrom(e.target.value);
+  };
+
+  const expr = "Currency";
+  return (
+    <section>
+      {!fetchingFrom && <div>Wybierz co Cię interesuje</div>}
+      <div>
+        <button value={"Gold"} onClick={setWhatYouWant}>
+          Złoto
+        </button>
+        <button value={"Currency"} onClick={setWhatYouWant}>
+          Waluty
+        </button>
+      </div>
+
+      {(() => {
+        switch (fetchingFrom) {
+          case "Gold":
+            return <div>Gold</div>;
+          case "Currency":
+            return <div>Currency</div>;
+          default:
+            return null;
+        }
+      })()}
+    </section>
+  );
 };
 
 export default NBPApi;
