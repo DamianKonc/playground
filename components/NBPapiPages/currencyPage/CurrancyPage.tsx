6db752faf30e4a85fd109cf077c2c378d0f2fa86
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 <damian.koncewicz@wp.pl>
2022-08-28 11:02:09 +0200

    styling nbp api

--- components/NBPapiPages/currencyPage/CurrancyPage.tsx
+++ components/NBPapiPages/currencyPage/CurrancyPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from "./currencyPage.module.scss";
 
 const CurrancyPage = () => {
   const [currencyRate, setCurrencyRate] = useState<any[]>([]);
@@ -16,28 +17,37 @@ const CurrancyPage = () => {
   console.log(currencyRate);
 
   return (
-    <section>
+    <section className={styles.currencyPage}>
       <h1>I AM CURRANCY</h1>
       {loading ? (
         <div>LOADING</div>
       ) : (
-        <>
-          {" "}
+        <div className={styles.currencyPage__dataWrapper}>
           <h2>{currencyRate[0].effectiveDate}</h2>
           <h3>{currencyRate[0].no}</h3>
-          <div>{currencyRate[0].table}</div>
-          <div>
+          <div>Table type: {currencyRate[0].table}</div>
+          <div className={styles.currencyPage__dataWrapper_datas}>
             {currencyRate[0].rates.map((el) => (
-              <div key={el.code}>
-                <span>
-                  {el.currency}
-                  {el.code}
-                  {el.mid}
-                </span>{" "}
+              <div
+                className={styles.currencyPage__dataWrapper_datas_el}
+                key={el.code}
+              >
+                <span className={styles.currencyPage__dataWrapper_datas_el_fat}>
+                  Currency:
+                </span>
+                {el.currency}
+                <span className={styles.currencyPage__dataWrapper_datas_el_fat}>
+                  Code:
+                </span>
+                {el.code}
+                <span className={styles.currencyPage__dataWrapper_datas_el_fat}>
+                  Mid price:
+                </span>
+                {el.mid} zł
               </div>
             ))}
           </div>
-        </>
+        </div>
       )}
     </section>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,47 +3,9 @@ import { EventHandler, MouseEventHandler, ReactElement, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const [items, setItems] = useState<{ id: number; text: string }[]>([]);
-
-  const [inputText, setInputText] = useState("");
-
-  const handleClick = () => {
-    const newId = Math.random();
-    setItems((prev) => [{ id: newId, text: inputText }, ...prev]);
-    setInputText("");
-  };
-
-  const handleInputText = (e: React.FormEvent<HTMLInputElement>) => {
-    setInputText(e.currentTarget.value);
-  };
-
-  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const button: HTMLButtonElement = e.currentTarget;
-
-    setItems((prev) =>
-      prev.filter((el) =>
-        el.id !== parseFloat(button.dataset.id!) ? el : null
-      )
-    );
-  };
   return (
     <>
       <div role={"home"}>Home</div>
-      <button onClick={handleClick}>Guzik</button>
-      <input onChange={handleInputText} value={inputText}></input>
-      <ul>
-        {items
-          ? items.map((item) => (
-              <li key={item.id}>
-                {item.text}
-                <button data-id={item.id} onClick={handleDelete}>
-                  Usuń mnie
-                </button>
-              </li>
-            ))
-          : null}
-      </ul>
     </>
   );
 };
