7ce1aee49ca25dc44d1b92049311b9ffc36f176b
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 <damian.koncewicz@wp.pl>
2022-08-28 09:53:34 +0200

    adding currency to site

--- components/NBPapiPages/currencyPage/CurrancyPage.tsx
+++ components/NBPapiPages/currencyPage/CurrancyPage.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+
 const CurrancyPage = () => {
-  return <h1>I AM CURRANCY</h1>;
+  const [currencyRate, setCurrencyRate] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://api.nbp.pl/api/exchangerates/tables/A/")
+      .then((data) => data.json())
+      .then((json) => {
+        setLoading(false);
+        setCurrencyRate(json);
+      });
+  }, []);
+
+  console.log(currencyRate);
+
+  return (
+    <section>
+      <h1>I AM CURRANCY</h1>
+      {loading ? (
+        <div>LOADING</div>
+      ) : (
+        <>
+          {" "}
+          <h2>{currencyRate[0].effectiveDate}</h2>
+          <h3>{currencyRate[0].no}</h3>
+          <div>{currencyRate[0].table}</div>
+          <div>
+            {currencyRate[0].rates.map((el) => (
+              <div key={el.code}>
+                <span>
+                  {el.currency}
+                  {el.code}
+                  {el.mid}
+                </span>{" "}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default CurrancyPage;
